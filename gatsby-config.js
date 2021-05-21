e99fdc7c9e08e2b24e7a28bf8ba2ec36e270522f
Evgeny Kasian <ekasian@tuta.io>
2021-05-21 15:44:10 -0700

    Finished basic tailwind css setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
     {
       resolve: "gatsby-source-filesystem",
       options: {
