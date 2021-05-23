6c23029366f5911741a137e2d080943d8aae9431
Evgeny Kasian <ekasian@tuta.io>
2021-05-23 12:30:48 -0700

    Added tailwinds jit build engine

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 const IndexPage = () => {
-  return <h1 className="text-primary text-9xl font-headline">Hello</h1>;
+  return <h1 className="text-secondary text-base font-headline">Hello</h1>;
 };
 
 export default IndexPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx,vue}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
