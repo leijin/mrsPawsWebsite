a3dbbe1a30a5fc87cc16702be8dd1932c72d0f6a
Evgeny Kasian <ekasian@tuta.io>
2021-05-29 16:18:21 -0700

    Hero Banner looks good on all devices

--- src/components/Button.js
+++ src/components/Button.js
@@ -8,7 +8,7 @@ export default function Button({
   return (
     <button
       type="button"
-      className={`${className} inline-flex items-center px-2 py-1 border border-transparent font-primary text-lg font-normal rounded-md shadow-sm text-white bg-secondary hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400 lg:px-4 lg:py-2 lg:text-xl`}
+      className={`${className} inline-flex items-center px-2 py-1 border border-transparent font-primary text-lg font-normal rounded-md shadow-sm text-white bg-secondary hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400 lg:px-4 lg:py-2 lg:text-xl 2xl:px-5 2xl:py-3`}
     >
       <a href={href}>{children}</a>
     </button>
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -5,14 +5,14 @@ import BannerImage from "../images/hero_banner.jpg";
 export function HeroBanner() {
   return (
     <>
-      <div className="w-[47.5%] bg-primary-dark bg-opacity-60 h-52 absolute container flex flex-col justify-center">
-        <p className="text-white font-heroHeadline font-medium text-4xl leading-[2.85rem]">
+      <div className="w-[47.5%] bg-primary-dark bg-opacity-60 absolute flex flex-col justify-center items-center h-52 md:h-60 lg:h-[32rem] 2xl:h-[50rem]">
+        <p className="text-white font-heroHeadline font-medium text-4xl leading-[2.85rem] max-w-[150px] md:text-5xl md:max-w-[270px] lg:text-6xl lg:max-w-full 2xl:text-[5rem]">
           Come For An Adventure With Us
         </p>
       </div>
       <img
         src={BannerImage}
-        className="w-full h-52 object-center object-cover"
+        className="w-full object-center object-cover h-52 md:h-60 lg:h-[32rem] 2xl:h-[50rem]"
       />
     </>
   );
