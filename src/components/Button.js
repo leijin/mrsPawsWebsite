d7dd5d33fcc33c07d27e4ef6ff9df353df5ff54e
Evgeny Kasian <ekasian@tuta.io>
2022-02-19 21:43:29 +0100

    Making sure the whole button is wrapped in link

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,12 +6,14 @@ export function Button({
   href = "https://www.timetopet.com/portal/mrspawspets",
 }) {
   return (
-    <button
-      type="button"
-      className={`${className} inline-flex items-center px-2 py-1 border border-transparent font-primary text-lg font-normal rounded-md shadow-sm text-white bg-secondary hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400 md:px-3 lg:px-4 lg:py-2 lg:text-xl 2xl:px-5 2xl:py-3`}
-    >
-      <a href={href}>{children}</a>
-    </button>
+    <a href={href}>
+      <button
+        type="button"
+        className={`${className} inline-flex items-center px-2 py-1 border border-transparent font-primary text-lg font-normal rounded-md shadow-sm text-white bg-secondary hover:bg-red-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-400 md:px-3 lg:px-4 lg:py-2 lg:text-xl 2xl:px-5 2xl:py-3`}
+      >
+        {children}
+      </button>
+    </a>
   );
 }
 
